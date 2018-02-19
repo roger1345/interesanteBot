d244d62d0aa8f242d357ab06b38cad4da8d77471
Roger <roger1345@gmail.com>
2018-02-18 21:15:14 -0300

    adding comments

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ var topicSchema = mongoose.Schema({
 
 var Topic = mongoose.model('Topic', topicSchema);
     
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
